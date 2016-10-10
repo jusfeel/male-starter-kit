67f56c916dfe68e439d75e130bcfb6a010a5cf6f
Hao <jusfeel@163.com>
2016-10-10 15:57:20 +0800

    embeding system bug and clean up

--- app/pods/entry/serializer.js
+++ app/pods/entry/serializer.js
@@ -2,7 +2,8 @@ import DS from 'ember-data';
 
 export default DS.JSONSerializer.extend(DS.EmbeddedRecordsMixin, {
   attrs: {
-    country: { embedded: 'always' }
+    country: { embedded: 'always' },
+    system: { embedded: 'always' }
   },
   primaryKey: '_id'
 });
--- app/pods/user/route.js
+++ app/pods/user/route.js
@@ -4,20 +4,20 @@ import RSVP from 'rsvp';
 export default Ember.Route.extend({
   model(params) {
     const userId = params.user_id;
-    if(userId !== "secret" && userId == "wanghao") {
+    if(userId === "wanghao") {
       return RSVP.hash({
         entries: this.get('store').findAll('entry'),
         countries: this.get('store').findAll('country'),
         systems: this.get('store').findAll('system')
       });
-    }else if(userId === "admin") {
+    }else if(userId === "secret") {
       this.transitionTo('index');
     }else{
       this.transitionTo('about');
     }
   },
 
-  afterModel: function(posts, transtion) {
+  afterModel: function() {
     this.transitionTo('search');
   }
 });
